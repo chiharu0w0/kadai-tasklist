50d14dde2717c11f771fa1bf8a43d83488c0839c
kussy <kussy.sample@gmail.com>
2019-07-24 12:44:11 +0000

    add user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,21 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      # @tasks = Task.where(user_id: current_user.id).order(id: :desc).page(params[:page])
+    end
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が登録されませんでした'
       render :new
     end
@@ -23,7 +30,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(params[:id])
+     @task = Task.find(params[:id])
   end
   
   def update
@@ -38,11 +45,10 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    # redirect_back(fallback_location: root_path)
+    redirect_to root_url
   end
   
   private
@@ -50,4 +56,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_url
+    end
+  end
 end
